0e8dac5c0d156dd04f93328894569f9a20706c15
Aleksandr Karpov <papadoster13@gmail.com>
2023-02-19 20:42:22 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Александр Карпов on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
